fc3835e415fb026b284db52ececfde456529e5a3
nazwaagustina <agustinanazwa202@gmail.com>
2022-11-18 16:15:52 +0700

    first commit

---
+++ index2.php
@@ -0,0 +1,51 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Portofolio</title>
+    <font size="+6" color="black" face="comicsans">
+        <center>
+    <h1> Portofolio </h1>
+    </center>
+    </font>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
+   <img src="img/Mine1.jpg"width="300px"; height="400px"/>
+   <h1>Karakter</h1>
+   <font size="+3" color="black" face="comicsans">
+   <ul type="disc">
+    <li>Baik</li>
+    <li>Sopan</li>
+    <li>Jujur</li>
+    <li>Bertanggung jawab</li>
+    <li>Humoris</li>
+   </ul>
+   </font>
+   <h1>Pendidikan</h1> 
+   <font size="+3" color="black" face="comicsans">
+   <ul type="disc">
+    <li>Sekolah Dasar/SD            : Mis Al Falah jaksel </li>
+    <li>Sekolah Menengah Pertama/SMP: MTS Jamiatul Muta'limin </li>
+   </ul>
+  </body>
+</html>
+<style>
+  .whitetext {
+      color: rgb(0, 0, 0);
+  }
+  </style>
+  </head>
+  <body>
+  
+  
+  <style type="text/css">
+        body {background-color:rgb(238, 215, 10);}
+        h2 {color:rgb(5, 5, 5);}
+        p {color:rgb(3, 3, 3);}
+     </style>
+     
+  
+  </style>
